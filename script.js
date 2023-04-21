ef9799b1233819a5d7333042effeeaf6b6b3f259
Japapc <carloshenriquefnp@gmail.com>
2023-04-21 17:24:54 -0300

    Version 1.0.1

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 const catalog = [
   {
     nome: "Peça 1",
-    descricao: "Descrição da Peça 1",
-    localizacao: "Carro",
-    imagem: "https://via.placeholder.com/150",
-  },
-  {
-    nome: "Peça 2",
-    descricao: "Descrição da Peça 2",
-    localizacao: "Trator",
-    imagem: "https://via.placeholder.com/150",
-  },
-  {
-    nome: "Peça 3",
     descricao: "Descrição da Peça 3",
-    localizacao: "Carro",
+    grupo: "Carro",
+    localizacao: "333",
     imagem: "https://via.placeholder.com/150",
   },
   {
-    nome: "Peça 4",
-    descricao: "Descrição da Peça 4",
-    localizacao: "Trator",
+    nome: "Peça 1",
+    descricao: "Descrição da Peça 3",
+    grupo: "Carro",
+    localizacao: "333",
     imagem: "https://via.placeholder.com/150",
   },
+  {
+    nome: "Peça 1",
+    descricao: "Descrição da Peça 3",
+    grupo: "Carro",
+    localizacao: "21A11C02",
+    imagem: "./images/rolamento.jpg",
+  },
+
 ];
 
 const tableBody = document.getElementById("table-body");
 const navLinks = document.querySelectorAll("nav a");
 
-function updateTable(localizacao) {
+function updateTable(grupo) {
   tableBody.innerHTML = "";
   catalog.forEach((peca) => {
-    if (peca.localizacao === localizacao) {
+    if (peca.grupo === grupo) {
       const row = `
         <tr>
           <td>${peca.nome}</td>
           <td>${peca.descricao}</td>
+          <td>${peca.grupo}</td>
           <td>${peca.localizacao}</td>
-          <td><img src="${peca.imagem}" alt="Imagem da peça ${peca.nome}"></td>
+          <td><img src="${peca.imagem}" alt="Imagem da peça ${peca.nome}" height="150" width="150"></td>
         </tr>
       `;
       tableBody.innerHTML += row;
